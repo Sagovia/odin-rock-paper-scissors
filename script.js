26e7dbbaf50e04f427b23306180e2f4cadaa85c6
Sagovia <137645512+Sagovia@users.noreply.github.com>
2024-06-07 17:46:49 -0400

    Finished the game by adding the playGame() function

--- script.js
+++ script.js
@@ -18,52 +18,72 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-    if(humanChoice === computerChoice) {
-        console.log("Tie! Try again.")
-        return;
-    }
-    let humanWon = false;
 
-    switch(humanChoice) {
-        case "rock":
-            switch(computerChoice) {
-                case "scissors":
-                    humanWon = true;
-                    break;
+
+function playGame(){
+
+    function playRound(humanChoice, computerChoice) {
+        if(humanChoice === computerChoice) {
+            console.log("Tie! Try again.")
+            return;
+        }
+        let humanWon = false;
+    
+        switch(humanChoice) {
+            case "rock":
+                switch(computerChoice) {
+                    case "scissors":
+                        humanWon = true;
+                        break;
+                }
+                break;
+            case "paper":
+                switch(computerChoice) {
+                    case "rock":
+                        humanWon = true;
+                        break;
+                }
+                break;
+            case "scissors": 
+                switch(computerChoice) {
+                    case "paper": 
+                        humanWon = true;
+                        break;
+                break;
             }
-            break;
-        case "paper":
-            switch(computerChoice) {
-                case "rock":
-                    humanWon = true;
-                    break;
-            }
-            break;
-        case "scissors": 
-            switch(computerChoice) {
-                case "paper": 
-                    humanWon = true;
-                    break;
-            break;
+        }
+    
+        if(humanWon) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        }
+        else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+            computerScore++;
         }
     }
 
-    if(humanWon) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
+    let humanScore = 0;
+    let computerScore = 0;
+
+
+    for(i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    if(humanScore > computerScore) {
+        console.log("Congrats, you won the game!");
+    }
+    else if(humanScore < computerScore) {
+        console.log("Sorry, you lost the game.");
     }
     else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-        computerScore++;
+        console.log("The game was a tie overall!");
     }
 }
 
+playGame();
 
-let humanScore = 0;
-let computerScore = 0;
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
