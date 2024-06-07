33227de1cd293f83b71ab2fe9db79c7548ec7071
Sagovia <137645512+Sagovia@users.noreply.github.com>
2024-06-07 15:28:36 -0400

    added getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("test");
+function getComputerChoice() {
+    let randomNum = Math.floor((Math.random() * 3) + 1);
+    switch(randomNum) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    }
+}
+
+
